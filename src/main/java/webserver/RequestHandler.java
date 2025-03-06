ac07af1f3b059842e78db6113b140a810acdba38
jihoon <wlgnstls0413@naver.com>
2025-03-06 11:03:05 +0900

    feat: CH03 요구사항 6
    사용자 목록 출력

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,6 +3,8 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
 
@@ -10,6 +12,7 @@ import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 import util.IOUtils;
 
 public class RequestHandler extends Thread {
@@ -36,12 +39,17 @@ public class RequestHandler extends Thread {
 
             String[] tokens = line.split(" ");
             int contentLength = 0;
+            boolean logined = false;
 
             while (!line.isEmpty()) {
                 line = br.readLine();
                 log.info("http Header : {} ", line);
                 if(line.contains("Content-Length")) {
                     contentLength = Integer.parseInt(line.split(":")[1].trim());
+                }
+                if(line.contains("Cookie")) {
+                    logined = isLogin(line);
+                    log.info("logined >>>>>>>>>>> {}", logined);
                 }
             }
 
@@ -62,20 +70,52 @@ public class RequestHandler extends Thread {
                 Map<String, String> params = util.HttpRequestUtils.parseQueryString(body);
                 User user = DataBase.findUserById(params.get("userId"));
                 DataOutputStream dos = new DataOutputStream(out);
-                String cookie = "logined=false";
                 if(user != null && Objects.equals(user.getPassword(), params.get("password"))) {
                     response302LoginSuccessHeader(dos);
                 } else {
                     responseResource(out, "/user/login_failed.html");
                 }
-            }
-            else {
+            } else if (url.equals("/user/list.html")) {
+                if(!logined) {
+                    responseResource(out, "/user/login.html");
+                    return;
+                }
+
+                Collection<User> users = DataBase.findAll();
+                StringBuilder sb = new StringBuilder();
+                sb.append("<table border='1'>");
+                for (User user : users) {
+                    sb.append("<tr>");
+                    sb.append("<td>").append(user.getUserId()).append("</td>");
+                    sb.append("<td>").append(user.getName()).append("</td>");
+                    sb.append("<td>").append(user.getEmail()).append("</td>");
+                    sb.append("</tr>");
+                }
+                sb.append("</table>");
+
+                byte[] body = sb.toString().getBytes();
+                DataOutputStream dos = new DataOutputStream(out);
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            } else {
                 responseResource(out, url);
             }
 
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private boolean isLogin(String line) {
+        String temp = line.split(":")[1].trim();
+        Map<String, String> cookies = HttpRequestUtils.parseCookies(temp);
+
+        String value = cookies.get("logined");
+        if(value == null) {
+            return false;
+
+        }
+        return Boolean.parseBoolean(cookies.get("logined"));
     }
 
     private void responseResource(OutputStream out , String url) throws IOException {
